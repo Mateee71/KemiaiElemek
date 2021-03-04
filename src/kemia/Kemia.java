0ef203452f6731adeab1ebdc2b6ccc58e7842caa
RichardHuszar <huszirics10@gmail.com>
2021-03-04 11:24:42 +0100

    3. feladat

--- src/kemia/Kemia.java
+++ src/kemia/Kemia.java
@@ -5,20 +5,28 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Kemia {
 
     public static void main(String[] args) throws IOException {
-        
+
         List<String> sorok = Files.readAllLines(Paths.get("felfedezesek.csv"));
-        
+
         ArrayList<Elem> elemek = new ArrayList<>();
-        for (int i = 0; i < sorok.size(); i++) {
+        for (int i = 1; i < sorok.size(); i++) {
             elemek.add(new Elem(sorok.get(i)));
         }
+
+        //3.feladat
+        System.out.println("3.feladat");
+        int elemdb = 0;
+        for (int i = 0; i < elemek.size(); i++) {
+            elemdb++;
+        }
+        System.out.println("Elemek szám: " + elemdb);
+
         
-        
-        
+
     }
-    
 }
