d2edb495ae4ce17c2fc08e4491b1633f3393f51b
MateKovacs <netmate12@gmail.com>
2021-03-05 11:40:48 +0100

    7. feladat

--- src/kemia/Kemia.java
+++ src/kemia/Kemia.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Kemia {
 
@@ -13,9 +15,29 @@ public class Kemia {
         List<String> sorok = Files.readAllLines(Paths.get("felfedezesek.csv"));
         
         ArrayList<Elem> elemek = new ArrayList<>();
-        for (int i = 0; i < sorok.size(); i++) {
+        for (int i = 1; i < sorok.size(); i++) {
             elemek.add(new Elem(sorok.get(i)));
         }
+        
+        
+        System.out.print("7. feladat: ");
+        int maxEv = 0;
+        for (int i = 0; i < elemek.size()-1; i++) {
+            if (!(elemek.get(i).getEv().equals("Ókor"))) {
+                Integer.parseInt(elemek.get(i+1).getEv());
+                if ((Integer.parseInt(elemek.get(i+1).getEv()) - Integer.parseInt(elemek.get(i).getEv())) > maxEv) {
+                    maxEv = Integer.parseInt(elemek.get(i+1).getEv()) - Integer.parseInt(elemek.get(i).getEv());
+                }
+            }
+            
+        }
+        System.out.println(maxEv + " év volt a leghosszabb időszak két elem felfedezése között.");
+        
+        
+        
+        
+        
+        
         
         
         
